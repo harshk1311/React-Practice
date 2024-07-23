466c50d11f70c8ffde98bd1b34ad01f355bd8645
Harsh Kairamkonda <harshkairamkonda@gmail.com>
2024-07-23 23:31:01 +0530

    Using States

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import resObjectList from "../utils/mockdata";
 import RestaruntComponent from "./Restarunt";
 
 export const BodyComponent = ()=>{
+     const arr=useState(resObjectList);
+        console.log(arr)
+    //  const [resList,setResList]=arr;
+    const resList=arr[0];
+    const setResList=arr[1]
     return(
         <div className='body-part'>
 
             <div className='search'>
-                <h4>Search</h4>
+                <button className="top-btn" onClick={()=>{
+                    const filteredList = resObjectList.filter((res)=>res.info.avgRatingString>4.5)
+                    // console.log(filteredList)
+                    setResList(filteredList);
+                }}>Top Rating Restarunt</button>
             </div>
 
             <div className='res-container'>
                 {
-                    resObjectList.map(resObj => <RestaruntComponent key={resObj.info.id} resData={resObj} />)
+                    resList.map(resObj => <RestaruntComponent key={resObj.info.id} resData={resObj} />)
 
                 }
                 {/* <RestaruntComponent resData={resObjectList[0]} /> */}
